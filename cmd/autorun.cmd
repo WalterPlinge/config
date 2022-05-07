79b9ede42f11be0c77ec80b7b47ca1b5b4f8d6a1
WalterPlinge <22519813+WalterPlinge@users.noreply.github.com>
2022-05-08 00:53:10 +0100

    Update autorun.cmd

--- cmd/autorun.cmd
+++ cmd/autorun.cmd
@@ -1,3 +1,8 @@
+:: to automatically run this script, add it to the registry
+:: [HKEY_CURRENT_USER\SOFTWARE\Microsoft\Command Processor]
+:: "AutoRun"="path\to\autorun.cmd"
+:: or edit your cmd launcher to run it as a command
+:: "cmd.exe" /k "path\to\autorun.cmd"
 @echo off
 call "%CLINK_DIR%\clink.bat" inject --autorun --profile ~\clink
 call %~dp0\alias.cmd
