592710fdee320acd6e65adbd69fcbd6e046b326e
WalterPlinge <22519813+WalterPlinge@users.noreply.github.com>
2022-11-03 03:26:03 +0000

    modified gitl formatting

--- cmd/alias.cmd
+++ cmd/alias.cmd
@@ -9,15 +9,14 @@ doskey curldl=curl -JLO $*
 doskey ls=dir /OG $*
 doskey mv=move $*
 doskey rm=del $*
-doskey which=where $*
 
 :: some git helpers
 doskey gita=git add $*
 doskey gitc=git commit $*
 doskey gitd=git diff $*
 doskey gitf=git fetch $*
-doskey gitl=git log --graph --pretty=tformat:"%C(auto)%h%d%n%cN %cs (Author: %aN %as)%n  %s"
-doskey gitlb=git log -n 2 --graph --pretty=tformat:"%C(auto)%h%d%n%cN %cs (Author: %aN %as)%n  %s%n    %b"
+doskey gitl=git log $*
+doskey gitlp=git log --graph --pretty="format:%%C(yellow)%%h %%C(cyan)%%as %%C(green)%%aN %%C(auto)%%+d%%n%%w(64,4,4)%%s%%n" $*
 doskey gitm=git merge $*
 doskey gitp=git push $*
 doskey gits=git status $*
